7f4ab80bd53525fce6048a763511b2db861a077d
David Daniel <davedan7@gmail.com>
2015-08-05 22:31:22 -0600

    Turned board back into an object to make piece lookup quicker

--- test/board-test.js
+++ test/board-test.js
@@ -15,19 +15,58 @@ describe('The Board', function() {
   //   assert.equal(b.cols, 15);
   // });
 
-  it('letter tiles have different score', function () {
-    var b = new Board();
-    assert.equal(b.pieces["E"].score, 1);
-    assert.equal(b.pieces["Z"].score, 10);
+  // it('letter tiles have different score', function () {
+  //   var b = new Board();
+  //   assert.equal(b.pieces["E"].score, 1);
+  //   assert.equal(b.pieces["Z"].score, 10);
+  // });
+
+  // it('has 100 letter tiles', function () {
+  //   var b = new Board();
+  //   var pieceCount = _.reduce(b.pieces, function(sum, piece) {
+  //     return sum + piece.count;
+  //   }, 0);
+
+  //   assert.equal(pieceCount, 100);
+  // });
+
+  it('can place pieces', function () {
+    var board = new Board();
+    var bob   = new Player(board);
+
+    board.placeTile(0,0, bob);
+
+    assert(board.hasTile(0,0));
+    // debugger;
+    // assert.equal(_.size(board.placedTiles).length, 1);
   });
 
-  it('has 100 letter tiles', function () {
-    var b = new Board();
-    var pieceCount = _.reduce(b.pieces, function(sum, piece) {
-      return sum + piece.count;
-    }, 0);
+  it('The first placed piece must be in the middle of the board', function () {
+    var board = new Board();
+    var bob   = new Player(board);
 
-    assert.equal(pieceCount, 100);
+    board.placeTile(6,6, bob);
+    assert(!board.hasTile(6,6));
+
+    board.placeTile(0,0, bob);
+    board.placeTile(1,0, bob);
+
+    assert(board.hasTile(0,0));
+    assert(board.hasTile(1,0));
+  });
+
+  it('can only place tiles next to other tiles', function () {
+    var board = new Board();
+    var bob   = new Player(board);
+
+    board.placeTile(0,0, bob);
+    board.placeTile(2,2, bob);
+    
+    assert(!board.hasTile(2,2));
+
+    assert(board.hasTile(0,0));
+    board.placeTile(1,0, bob);
+    assert(board.hasTile(1,0));
   });
 
   it('can find tiles', function () {
--- test/player-test.js
+++ test/player-test.js
@@ -9,44 +9,6 @@ describe('Player', function() {
     assert(new Player());
   });
 
-  it('Players can place pieces starting in the middle of the board', function () {
-    var board = new Board();
-    var bob   = new Player(board);
-
-    board.placeTile(0,0, bob);
-
-    assert(board.hasTile(0,0));
-    // debugger;
-    // assert.equal(_.size(board.placedTiles).length, 1);
-  });
-
-  it('The first placed piece must be in the middle of the board', function () {
-    var board = new Board();
-    var bob   = new Player(board);
-
-    board.placeTile(6,6, bob);
-    assert(!board.hasTile(6,6));
-
-    board.placeTile(0,0, bob);
-    board.placeTile(1,0, bob);
-
-    assert(board.hasTile(0,0));
-    assert(board.hasTile(1,0));
-  });
-
-  it('can only place tiles next to other tiles', function () {
-    var board = new Board();
-    var bob   = new Player(board);
-
-    board.placeTile(0,0, bob);
-    board.placeTile(2,2, bob);
-    
-    assert(!board.hasTile(2,2));
-
-    assert(board.hasTile(0,0));
-    board.placeTile(1,0, bob);
-    assert(board.hasTile(1,0));
-  });
 
   it('can draw letters, always resulting in 7 letters for the player', function () {
     var bob = new Player();
